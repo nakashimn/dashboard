9208a4e13b430a360043cbfaff71a3e405727f9b
unknown <nakashimn@gmail.com>
2019-04-25 00:40:29 +0900

    fix:html_view,works

--- dashboard/dashboard/urls.py
+++ dashboard/dashboard/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 import dboard.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', 'dashboard.views.main')
+    path('', dboard.views.main)
 ]
--- dashboard/dboard/views.py
+++ dashboard/dboard/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
 def main(request):
-    return HttpResponse("HelloWorld!")
+    return render(request, "index.html")
